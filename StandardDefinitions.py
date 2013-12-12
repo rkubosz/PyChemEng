c56bd03da8e5fe231aada4f4eee686837edcdf1a
Marcus Bannerman <m.bannerman@gmail.com>
2013-12-12 15:16:37 +0000

    Fixed Standard definitions

--- StandardDefinitions.py
+++ StandardDefinitions.py
@@ -6,5 +6,5 @@ DryAir = Components({"N2":78.084, "O2":20.946,  "Ar":0.934, "CO2":0.0397, "Ne":0
 
 #Definitions taken from the GasEq program
 StandardHydrocarbonCombustionComponents = {"N2", "H2O", "CO2", "CO", "O2", "OH", "H", "O", "H2", "NO"}
-ExtendedHydrocarbonCombustionComponents = StandardCombustionComponents + {"HCO", "CH2O", "CH4", "CH3", "HO2", "NO2", "NH3", "NH2", "N", "HCN", "CN", "N2O", "C2", "CH"}
-RichSootingProducts = ExtendedHydrocarbonCombustionComponents + {"C2H4", "C2H2", "NH", "CH", "CH2", "C(S)"}
+ExtendedHydrocarbonCombustionComponents = StandardHydrocarbonCombustionComponents.union({"HCO", "CH2O", "CH4", "CH3", "HO2", "NO2", "NH3", "NH2", "N", "HCN", "CN", "N2O", "C2", "CH"})
+RichSootingProducts = ExtendedHydrocarbonCombustionComponents.union({"C2H4", "C2H2", "NH", "CH", "CH2", "C(S)"})
