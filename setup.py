5046ddcd3f7ee3e16391429fd2bfdcbc1cdb5e27
Marcus Bannerman <m.bannerman@gmail.com>
2014-05-04 13:17:27 +0100

    Moved to a more sensible layout

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 
-ext_modules = [Extension('chemeng.elementdata', ['chemeng/elementdata.pyx'], language='c++', include_dirs=['.']),
-               Extension('chemeng.thermodata', ['chemeng/thermodata.pyx'], language='c++', include_dirs=['.']),
-               Extension('chemeng.speciesdata', ['chemeng/speciesdata.pyx'], language='c++', include_dirs=['.']),
-               Extension('chemeng.antoinedata', ['chemeng/antoinedata.pyx'], language='c++', include_dirs=['.']),
-               Extension('chemeng.transportdata', ['chemeng/transportdata.pyx'], language='c++', include_dirs=['.']),
-               Extension('chemeng.components', ['chemeng/components.pyx'], language='c++', include_dirs=['.']),
-               Extension('chemeng.phase', ['chemeng/phase.pyx'], language='c++', include_dirs=['.']),
-               Extension('chemeng.reaction', ['chemeng/reaction.pyx'], language='c++', include_dirs=['.']),
-               Extension('chemeng.standarddefinitions', ['chemeng/standarddefinitions.pyx'], language='c++', include_dirs=['.']),
+ext_modules = [Extension('chemeng.elementdata', ['src/chemeng/elementdata.pyx'], language='c++', include_dirs=['.']),
+               Extension('chemeng.thermodata', ['src/chemeng/thermodata.pyx'], language='c++', include_dirs=['.']),
+               Extension('chemeng.speciesdata', ['src/chemeng/speciesdata.pyx'], language='c++', include_dirs=['.']),
+               Extension('chemeng.antoinedata', ['src/chemeng/antoinedata.pyx'], language='c++', include_dirs=['.']),
+               Extension('chemeng.transportdata', ['src/chemeng/transportdata.pyx'], language='c++', include_dirs=['.']),
+               Extension('chemeng.components', ['src/chemeng/components.pyx'], language='c++', include_dirs=['.']),
+               Extension('chemeng.phase', ['src/chemeng/phase.pyx'], language='c++', include_dirs=['.']),
+               Extension('chemeng.reaction', ['src/chemeng/reaction.pyx'], language='c++', include_dirs=['.']),
+               Extension('chemeng.standarddefinitions', ['src/chemeng/standarddefinitions.pyx'], language='c++', include_dirs=['.']),
                ]
 
 setup(
@@ -20,11 +20,12 @@ setup(
     author="M. Campbell Bannerman",
     author_email="m.bannerman@gmail.com",
     packages=['chemeng'],
+    package_dir={'chemeng':'src/chemeng'},
     data_files=[#('PyChemEng', ['tests/chemeng.py']),
-                ('PyChemEng/data', ['chemeng/data/antoine.inp',
-                                    'chemeng/data/mass.mas03round.txt',
-                                    'chemeng/data/isotopicCompositions.inp',
-                                    'chemeng/data/thermo.inp'])],
+                ('PyChemEng/data', ['src/chemeng/data/antoine.inp',
+                                    'src/chemeng/data/mass.mas03round.txt',
+                                    'src/chemeng/data/isotopicCompositions.inp',
+                                    'src/chemeng/data/thermo.inp'])],
     cmdclass = {'build_ext': build_ext},
     ext_modules = ext_modules
 )
